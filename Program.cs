19b2399ce9190718aaf5fbcb91c842ffda4846d8
Luiz Otávio <luizotaviomtbs@gmail.com>
2022-05-28 11:57:36 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cronômetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
